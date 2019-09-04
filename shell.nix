f02e3ae0858af3dab488410c27898e03bb3ac52e
Jean-Baptiste Giraudeau <jean-baptiste.giraudeau@iohk.io>
2019-09-04 10:36:28 +0200

    Add shell with scripts for a provided env.

--- shell.nix
+++ shell.nix
@@ -1,9 +1,59 @@
+with (import ./lib.nix);
 { withHoogle ? true
+, env ? null
 }:
 let
   default = import ./default.nix {};
+
+  scripts-env = pkgs.stdenv.mkDerivation {
+
+    name = "cardano-node-scripts-${env}";
+
+    buildInputs = with pkgs; with default; with environments.${env};
+      let
+        run-cardano-node-script = writeScriptBin "run-cardano-node" ''
+          #!${pkgs.runtimeShell}
+          exec ${scripts.${env}.node} $@
+        '';
+
+        edge-topology = builtins.toFile "${env}-topology-edge-only.yaml" (builtins.toJSON [
+            {
+              nodeId = 0;
+              nodeAddress =  {
+                addr = edgeHost;
+                port = edgePort;
+              };
+              producers = [];
+            }
+          ]);
+
+        env-opts =
+        ''--genesis-file ${genesisFile} \
+          --genesis-hash ${genesisHash} \
+          --topology ${edge-topology} \
+          --node-id "0"'';
+
+        submit-tx-script = writeScriptBin "submit-tx" ''
+          #!${pkgs.runtimeShell}
+          exec ${nix-tools.exes.cardano-node}/bin/cardano-cli real-pbft \
+            submit-tx \
+            ${env-opts} \
+            --tx $@
+        '';
+
+      in [
+        nix-tools.exes.cardano-node
+        run-cardano-node-script
+        submit-tx-script
+      ];
+    shellHook = ''
+      echo "Cardano-Node Demo" \
+      | ${pkgs.figlet}/bin/figlet -f banner -c \
+      | ${pkgs.lolcat}/bin/lolcat
+    '';
+  };
 in
-default.nix-tools._raw.shellFor {
+default.nix-tools.shellFor {
   packages    = ps: with ps; [ cardano-node ];
   withHoogle  = withHoogle;
   buildInputs =
@@ -14,4 +64,4 @@ default.nix-tools._raw.shellFor {
   (with default.nix-tools._raw._config._module.args.pkgs; [
     tmux
   ]);
-}
+} // { inherit scripts-env; }
