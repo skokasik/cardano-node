cbfd7d074976695c368780bf93e871dd61cc56b3
Kosyrev Serge <serge.kosyrev@iohk.io>
2019-12-19 14:32:05 +0300

    Very compact repro:  DCE-minimal, inlined guts of Setup.setupTrace_

--- cardano-node/app/cardano-cli.hs
+++ cardano-node/app/cardano-cli.hs
@@ -1,21 +1,27 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 
-import           Cardano.BM.Backend.Switchboard
-import qualified Cardano.BM.Configuration.Model as Config
-import           Cardano.BM.Setup
-import           Cardano.BM.Data.Trace
 import           Cardano.Prelude
 import           Control.Concurrent (threadDelay)
+import           Control.Concurrent.Async as Async
+import qualified Control.Concurrent.STM.TBQueue as TBQ
 import           System.Mem (performMajorGC)
 
 main :: IO ()
 main = do
-  _ :: (Trace IO Text, Switchboard Text)
-    <- flip setupTrace_ "" =<< Config.empty
+  queue <- atomically $ TBQ.newTBQueue 2048
+  let qProc = do
+        nlis <- atomically $ do
+                      r <- TBQ.flushTBQueue queue
+                      when (null r) retry
+                      return r
+        res <- mapM (const $ pure True) nlis
+        when (and res) $ qProc
+  dispatcher <- Async.async qProc
+
+  -- The Async.link is necessary
+  Async.link dispatcher
 
   threadDelay 1000
   performMajorGC
   threadDelay 1000
-  putStrLn ("waiting" :: Text)
-  threadDelay 100000000
