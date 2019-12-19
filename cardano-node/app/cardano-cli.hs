045fa018a5a412dd286e77497344d7c4a0940bc9
Kosyrev Serge <serge.kosyrev@iohk.io>
2019-12-19 14:59:56 +0300

    Filter out the STM deadlock exceptions

--- cardano-node/app/cardano-cli.hs
+++ cardano-node/app/cardano-cli.hs
@@ -1,11 +1,20 @@
-{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 
-import           Cardano.Prelude
+import           Cardano.Prelude hiding (trace)
 import           Control.Concurrent (threadDelay)
 import           Control.Concurrent.Async as Async
 import qualified Control.Concurrent.STM.TBQueue as TBQ
 import           System.Mem (performMajorGC)
+import           GHC.IO.Exception (BlockedIndefinitelyOnSTM)
+import           Control.Exception (Exception)
+import           Data.Typeable (cast, typeOf)
+
+import Prelude (error)
+import Debug.Trace (trace)
+
+isBlockedIndefinitelyOnSTM :: SomeException -> Bool
+isBlockedIndefinitelyOnSTM (SomeException e) =
+  isJust (cast e :: Maybe BlockedIndefinitelyOnSTM)
 
 main :: IO ()
 main = do
@@ -19,8 +28,7 @@ main = do
         when (and res) $ qProc
   dispatcher <- Async.async qProc
 
-  -- The Async.link is necessary
-  Async.link dispatcher
+  Async.linkOnly (not . isBlockedIndefinitelyOnSTM) dispatcher
 
   threadDelay 1000
   performMajorGC
