58fdbdf3995d536c110e5513a391749d0007dc38
Craige McWhirter <craige.mcwhirter@iohk.io>
2020-01-31 16:00:19 +1000

    Added start script

--- nix/docker.nix
+++ nix/docker.nix
@@ -23,6 +23,15 @@
 
 let
 
+  startScript = writeScriptBin "start-cardano-node" ''
+    #!${runtimeShell}
+     set -euo pipefail
+
+     mkdir -p /data /config
+
+     exec ${cardano-node}/bin/cardano-node --config /config/config.yaml --database-path /data/db --genesis-file /config/genesis.json --host-addr 127.0.0.1 --port 3001 --socket-dir /data/ipc/socket --topology /config/topology.json "$@"
+  '';
+
   # Layer of tools which aren't going to change much between versions.
   baseImage = dockerTools.buildImage {
     name = "${repoName}-env";
@@ -38,27 +47,17 @@ in
   dockerTools.buildImage {
     name = repoName;
     ### tag = "${cardano-node.version}";
-    tag = "yolo";
+    tag = "tag04";
     fromImage = baseImage;
     contents = [
       cardano-node
+      startScript
     ];
     created = "now";   # Set creation date to build time. Breaks reproducibility
     config = {
-      EntryPoint = ''
-        "cardano-node","--config","/configuration/config.yaml",
-        "--database-path","/db","--genesis-file","/configuration/genesis.json",
-        "--host-addr","127.0.0.1","--port","3001","--socket-dir","/socket",
-        "--topology","/configuration/topology.yaml"
-      '';
+      EntryPoint = [ "start-cardano-node" ];
       ExposedPorts = {
         "3001/tcp" = {};  # Cardano node to node port
-      };
-      WorkingDir = "/cardano-node";
-      Volumes = {
-        "/cardano-node" = {};
-        "node-db:/db" = {};
-        "node-socket:/socket" = {};
       };
     };
   }  #### // { inherit (cardano-node) version; }
